f9df26b2807963471327ca9a7eab6cbf27ae469f
Tim Arney <tim@line37.com>
2019-07-03 10:24:14 -0400

    add bad vetsion of mergeWith

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const flatten = require("lodash.flatten");
+const mergeWith = require("lodash.mergewith");
+
+const other = [1, 2, 5];
+
 const arr = [1, [1, 2], 3, 4];
 
 const moduleName = "module 2";
 
 const print = () => {
   console.log(`== ${moduleName} ==`);
-  console.log(flatten(arr));
+  console.log("flatten & merge")
+  console.log(mergeWith(flatten(arr),other));
   console.log("============");
 };
-
+print()
 module.exports.print = print;
